e484d234534a9d3a73e2e0605e41624ca4b89716
Luca Somaschini <somas99@gmail.com>
2022-09-05 17:08:26 +0200

    Section1 in main: completed

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -10,10 +10,26 @@
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, quis.</p>
         </div>
         <div class="s1_right d_flex">
-          <div class="square">quadrato1</div>
-          <div class="square">quadrato1</div>
-          <div class="square">quadrato1</div>
-          <div class="square">quadrato1</div>
+          <div class="square">
+            <i class="fa-solid fa-medal"></i>
+            <h2>Tradition</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, necessitatibus.</p>
+          </div>
+          <div class="square">
+            <i class="fa-solid fa-lock"></i>
+            <h2>Security</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Necessitatibus, animi?</p>
+          </div>
+          <div class="square">
+            <i class="fa-regular fa-note-sticky"></i>
+            <h2>Certificate</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, voluptatibus!</p>
+          </div>
+          <div class="square">
+            <i class="fa-solid fa-graduation-cap"></i>
+            <h2>Expertise</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam, alias?</p>
+          </div>
         </div>
       </div>
     </section>
@@ -41,11 +57,12 @@ export default {
   color: #fff; //eliminare
 
   .container {
-    justify-content: center;
+    justify-content: space-between;
     padding: 20px;
 
     .s1_left {
       max-width: 50%;
+      margin-right: 15px;
 
       h5 {
         color: $c5;
@@ -63,10 +80,27 @@ export default {
 
     .s1_right {
       max-width: 50%;
+      margin-left: 15px;
       flex-wrap: wrap;
 
       .square {
-        flex-basis: calc(100% / 2);
+        flex-basis: calc(100% / 2 - 10px);
+        margin: 5px;
+
+        i {
+          margin-right: 10px;
+          color: $c2;
+          font-size: 1.1rem;
+        }
+
+        h2 {
+          display: inline-block;
+        }
+
+        p {
+          margin: 10px 0;
+          color: grey;
+        }
       }
     }
   }
