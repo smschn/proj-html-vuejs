1496267a01a56f3561481c53b324a3fd2ff8c1b0
Luca Somaschini <somas99@gmail.com>
2022-09-06 09:01:36 +0200

    Added section3 component + dynamically created data

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -2,22 +2,11 @@
   <main>
     <MySection1 />
     <MySection2 />
-    <section id="section3">
-      <div class="container s3_first">
-        <h5>how it works in practice</h5>
-        <h1><span>Process</span> Management</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, reprehenderit quasi. Facilis, suscipit dolor voluptatum minima ea fugit exercitationem nam!</p>
-      </div>
-      <div class="s3_second">
-        <div class="circle_card_container" v-for="(process, index) in processesData" v-bind:key="index">
-          <div class="circle">{{process.number}}</div>
-          <h3>{{process.title}}</h3>
-          <p>{{process.text}}</p>
-        </div>
-      </div>
-    </section>
+    <MySection3 />
     <MySection4 />
-    <section id="section5"></section>
+    <section id="section5">
+
+    </section>
     <section id="section6"></section>
   </main>
 </template>
@@ -25,46 +14,16 @@
 <script>
 import MySection1 from './MySection1.vue';
 import MySection2 from './MySection2.vue';
+import MySection3 from './MySection3.vue';
 import MySection4 from './MySection4.vue';
-
 
 export default {
     name: 'MyMain',
     components: {
       MySection1,
       MySection2,
+      MySection3,
       MySection4
-    },
-    data() {
-      return {
-        processesData: [
-          {
-            number: '01',
-            title: 'Collection of information',
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
-          },
-          {
-            number: '02',
-            title: 'Strategic planning',
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
-          },
-          {
-            number: '03',
-            title: 'Assignment of responsibilities',
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
-          },
-          {
-            number: '04',
-            title: 'Formatting process',
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
-          },
-          {
-            number: '05',
-            title: 'Continuity formalization',
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
-          },
-        ]
-      }
     }
 }
 </script>
@@ -73,71 +32,7 @@ export default {
 @import '../style/vars.scss';
 @import '~@fortawesome/fontawesome-free/css/all.css';
 
-#section3 {
-  padding: 100px;
+#section5 {
 
-  .container.s3_first {
-    text-align: center;
-
-    h5 {
-      text-transform: uppercase;
-      color: $c5;
-    }
-
-    h1 {
-      margin: 20px 0;
-      font-size: 3rem;
-
-      span {
-      background-color: $c4;
-      color: $c2;
-      border-radius: 5px;
-      padding: 10px;
-      }
-    }
-
-    p {
-      color: grey;
-    }
-  }
-
-  .s3_second {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    margin-top: 50px;
-
-    .circle_card_container {
-      flex-basis: calc(100% / 5);
-      text-align: center;
-      border-top: 4px solid $c4;
-      position: relative;
-
-      .circle {
-        width: 40px;
-        height: 40px;
-        border-radius: 50%;
-        background-color: $c4;
-        position: absolute;
-        left: 50%;
-        transform: translateX(-50%);
-        top: -22px;
-        text-align: center;
-        line-height: 40px;
-        color: $c2;
-        font-weight: bold;
-      }
-
-      h3 {
-        margin: 30px 0;
-      }
-
-      p {
-        color: grey;
-      }
-    }
-
-
-  }
 }
 </style>
---
+++ src/components/MySection3.vue
@@ -0,0 +1,124 @@
+<template>
+    <section id="section3">
+      <div class="container s3_first">
+        <h5>how it works in practice</h5>
+        <h1><span>Process</span> Management</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, reprehenderit quasi. Facilis, suscipit dolor voluptatum minima ea fugit exercitationem nam!</p>
+      </div>
+      <div class="s3_second">
+        <div class="circle_card_container" v-for="(process, index) in processesData" v-bind:key="index">
+          <div class="circle">{{process.number}}</div>
+          <h3>{{process.title}}</h3>
+          <p>{{process.text}}</p>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'MySection3',
+    data() {
+      return {
+        processesData: [
+          {
+            number: '01',
+            title: 'Collection of information',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
+          },
+          {
+            number: '02',
+            title: 'Strategic planning',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
+          },
+          {
+            number: '03',
+            title: 'Assignment of responsibilities',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
+          },
+          {
+            number: '04',
+            title: 'Formatting process',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
+          },
+          {
+            number: '05',
+            title: 'Continuity formalization',
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing.'
+          },
+        ]
+      }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/vars.scss';
+@import '~@fortawesome/fontawesome-free/css/all.css';
+
+#section3 {
+  padding: 100px;
+
+  .container.s3_first {
+    text-align: center;
+
+    h5 {
+      text-transform: uppercase;
+      color: $c5;
+    }
+
+    h1 {
+      margin: 20px 0;
+      font-size: 3rem;
+
+      span {
+      background-color: $c4;
+      color: $c2;
+      border-radius: 5px;
+      padding: 10px;
+      }
+    }
+
+    p {
+      color: grey;
+    }
+  }
+
+  .s3_second {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    margin-top: 50px;
+
+    .circle_card_container {
+      flex-basis: calc(100% / 5);
+      text-align: center;
+      border-top: 4px solid $c4;
+      position: relative;
+
+      .circle {
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        background-color: $c4;
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        top: -22px;
+        text-align: center;
+        line-height: 40px;
+        color: $c2;
+        font-weight: bold;
+      }
+
+      h3 {
+        margin: 30px 0;
+      }
+
+      p {
+        color: grey;
+      }
+    }
+  }
+}
+</style>
