5c7b6994282fe2feed0e9fd4fe7a3cf570008196
Luca Somaschini <somas99@gmail.com>
2022-09-06 10:37:10 +0200

    Section5 in main: completed

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -9,8 +9,22 @@
         <h5>our editorial content</h5>
         <h1>Latest <span>News</span></h1>
         <div class="floated">
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum vitae rerum magni? Provident magni nostrum dolore doloribus vel. Tempora provident veritatis dolore tenetur dicta ratione ea minima eveniet rem qui.</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus repellat eius quaerat voluptatem est dolorem!</p>
           <button class="btn btn_green">SEE ALL</button>
+        </div>
+      </div>
+      <div class="container s5_bottom d_flex">
+        <div class="news_card d_flex">
+          <img src="../assets/img/news-1.jpg" alt="aaa">
+          <p>Increasing creativity is possibile for everyone</p>
+        </div>
+        <div class="news_card d_flex">
+          <img src="../assets/img/news-2.jpg" alt="aaa">
+          <p>Increasing creativity is possibile for everyone</p>
+        </div>
+        <div class="news_card d_flex">
+          <img src="../assets/img/news-3.jpg" alt="aaa">
+          <p>Increasing creativity is possibile for everyone</p>
         </div>
       </div>
     </section>
@@ -31,6 +45,24 @@ export default {
       MySection2,
       MySection3,
       MySection4
+    },
+    data() {
+      return {
+        newsData: [
+          {
+            imgSrc: require('../assets/img/news-1.jpg'),
+            text: 'Increasing creativity is possibile for everyone'
+          },
+          {
+            imgSrc: require('../assets/img/news-2.jpg'),
+            text: 'Because market research is part of the business plan'
+          },
+          {
+            imgSrc: require('../assets/img/news-3.jpg'),
+            text: 'Working from home is now a trend'
+          }
+        ]
+      }
     }
 }
 </script>
@@ -59,18 +91,54 @@ export default {
     }
   }
 
-  .floated:after {
-    content: "";
-    display: table;
-    clear: both;
+  .floated {
 
     p {
       float: left;
+      max-width: 70%;
+      color: grey;
     }
 
     button {
       float: right;
     }
   }
+
+  .floated:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+  .s5_bottom {
+    justify-content: space-around;
+    color: #fff;
+    font-weight: bold;
+
+    .news_card {
+      flex-basis: calc(100% / 3 - 5px);
+      max-height: 340px;
+      margin-top: 30px;
+      border-radius: 10px;
+      position: relative;
+      justify-content: center;
+
+
+      img {
+        max-width: 100%;
+        max-height: 100%;
+        filter: brightness(50%);
+        border-radius: 10px;
+      }
+
+      p {
+        position: absolute;
+        bottom: 20%;
+        text-align: center;
+        max-width: 70%;
+      }
+    }
+  }
+
 }
 </style>
