59d19940b62763e6056c5d2932ee3154c7e0b064
Luca Somaschini <somas99@gmail.com>
2022-09-06 10:43:52 +0200

    Added section5 component + dynamically created cards

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -4,30 +4,7 @@
     <MySection2 />
     <MySection3 />
     <MySection4 />
-    <section id="section5">
-      <div class="container">
-        <h5>our editorial content</h5>
-        <h1>Latest <span>News</span></h1>
-        <div class="floated">
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus repellat eius quaerat voluptatem est dolorem!</p>
-          <button class="btn btn_green">SEE ALL</button>
-        </div>
-      </div>
-      <div class="container s5_bottom d_flex">
-        <div class="news_card d_flex">
-          <img src="../assets/img/news-1.jpg" alt="aaa">
-          <p>Increasing creativity is possibile for everyone</p>
-        </div>
-        <div class="news_card d_flex">
-          <img src="../assets/img/news-2.jpg" alt="aaa">
-          <p>Increasing creativity is possibile for everyone</p>
-        </div>
-        <div class="news_card d_flex">
-          <img src="../assets/img/news-3.jpg" alt="aaa">
-          <p>Increasing creativity is possibile for everyone</p>
-        </div>
-      </div>
-    </section>
+    <MySection5 />
     <section id="section6"></section>
   </main>
 </template>
@@ -37,6 +14,7 @@ import MySection1 from './MySection1.vue';
 import MySection2 from './MySection2.vue';
 import MySection3 from './MySection3.vue';
 import MySection4 from './MySection4.vue';
+import MySection5 from './MySection5.vue';
 
 export default {
     name: 'MyMain',
@@ -44,25 +22,8 @@ export default {
       MySection1,
       MySection2,
       MySection3,
-      MySection4
-    },
-    data() {
-      return {
-        newsData: [
-          {
-            imgSrc: require('../assets/img/news-1.jpg'),
-            text: 'Increasing creativity is possibile for everyone'
-          },
-          {
-            imgSrc: require('../assets/img/news-2.jpg'),
-            text: 'Because market research is part of the business plan'
-          },
-          {
-            imgSrc: require('../assets/img/news-3.jpg'),
-            text: 'Working from home is now a trend'
-          }
-        ]
-      }
+      MySection4,
+      MySection5
     }
 }
 </script>
@@ -71,74 +32,5 @@ export default {
 @import '../style/vars.scss';
 @import '~@fortawesome/fontawesome-free/css/all.css';
 
-#section5 {
-  padding: 100px 0;
 
-  h5 {
-    text-transform: uppercase;
-    color: $c5;
-  }
-
-  h1 {
-    font-size: 3rem;
-    margin: 25px 0;
-
-    span {
-      background-color: $c4;
-      color: $c2;
-      border-radius: 5px;
-      padding: 10px;
-    }
-  }
-
-  .floated {
-
-    p {
-      float: left;
-      max-width: 70%;
-      color: grey;
-    }
-
-    button {
-      float: right;
-    }
-  }
-
-  .floated:after {
-    content: "";
-    display: table;
-    clear: both;
-  }
-
-  .s5_bottom {
-    justify-content: space-around;
-    color: #fff;
-    font-weight: bold;
-
-    .news_card {
-      flex-basis: calc(100% / 3 - 5px);
-      max-height: 340px;
-      margin-top: 30px;
-      border-radius: 10px;
-      position: relative;
-      justify-content: center;
-
-
-      img {
-        max-width: 100%;
-        max-height: 100%;
-        filter: brightness(50%);
-        border-radius: 10px;
-      }
-
-      p {
-        position: absolute;
-        bottom: 20%;
-        text-align: center;
-        max-width: 70%;
-      }
-    }
-  }
-
-}
 </style>
---
+++ src/components/MySection5.vue
@@ -0,0 +1,120 @@
+<template>
+    <section id="section5">
+      <div class="container">
+        <h5>our editorial content</h5>
+        <h1>Latest <span>News</span></h1>
+        <div class="floated">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus repellat eius quaerat voluptatem est dolorem!</p>
+          <button class="btn btn_green">SEE ALL</button>
+        </div>
+      </div>
+      <div class="container s5_bottom d_flex">
+        <div class="news_card d_flex" v-for="( news, index) in newsData" v-bind:key="index">
+          <img v-bind:src="news.imgSrc" v-bind:alt="news.alt">
+          <p>{{news.text}}</p>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'MySection5',
+    data() {
+      return {
+        newsData: [
+          {
+            imgSrc: require('../assets/img/news-1.jpg'),
+            alt: 'News1',
+            text: 'Increasing creativity is possibile for everyone'
+          },
+          {
+            imgSrc: require('../assets/img/news-2.jpg'),
+            alt: 'News2',
+            text: 'Because market research is part of the business plan'
+          },
+          {
+            imgSrc: require('../assets/img/news-3.jpg'),
+            alt: 'News3',
+            text: 'Working from home is now a trend'
+          }
+        ]
+      }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/vars.scss';
+@import '~@fortawesome/fontawesome-free/css/all.css';
+
+#section5 {
+  padding: 100px 0;
+
+  h5 {
+    text-transform: uppercase;
+    color: $c5;
+  }
+
+  h1 {
+    font-size: 3rem;
+    margin: 25px 0;
+
+    span {
+      background-color: $c4;
+      color: $c2;
+      border-radius: 5px;
+      padding: 10px;
+    }
+  }
+
+  .floated {
+
+    p {
+      float: left;
+      max-width: 70%;
+      color: grey;
+    }
+
+    button {
+      float: right;
+    }
+  }
+
+  .floated:after {
+    content: "";
+    display: table;
+    clear: both;
+  }
+
+  .s5_bottom {
+    justify-content: space-around;
+    color: #fff;
+    font-weight: bold;
+
+    .news_card {
+      flex-basis: calc(100% / 3 - 5px);
+      max-height: 340px;
+      margin-top: 30px;
+      border-radius: 10px;
+      position: relative;
+      justify-content: center;
+
+
+      img {
+        max-width: 100%;
+        max-height: 100%;
+        filter: brightness(50%);
+        border-radius: 10px;
+      }
+
+      p {
+        position: absolute;
+        bottom: 20%;
+        text-align: center;
+        max-width: 70%;
+      }
+    }
+  }
+}
+</style>
