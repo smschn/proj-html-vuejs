7b6ed4cfad548be816e6b0c4097d0acbfca49e4a
Luca Somaschini <somas99@gmail.com>
2022-09-06 14:55:49 +0200

    Added section2 component in footer

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,26 +1,19 @@
 <template>
     <footer>
         <MyFooterSection1 />
-        <section id="f2">
-            <div class="container f2 d_flex">
-                <div>
-                    Enjoy the low price. We are tracking any itnention of piracy.
-                </div>
-                <div>
-                    &copy; 2020 NEXGEN is Pourdly Powered by <a href="#">Codings</a>.
-                </div>
-            </div>
-        </section>
+        <MyFooterSection2 />
     </footer>
 </template>
 
 <script>
 import MyFooterSection1 from './MyFooterSection1.vue';
+import MyFooterSection2 from './MyFooterSection2.vue';
 
 export default {
     name: 'MyFooter',
     components: {
-        MyFooterSection1
+        MyFooterSection1,
+        MyFooterSection2
     }
 }
 </script>
@@ -28,20 +21,4 @@ export default {
 <style scoped lang="scss">
 @import '../style/vars.scss';
 @import '~@fortawesome/fontawesome-free/css/all.css';
-
-#f2 {
-    background-color: $c15;
-    color: $c1;
-    padding: 20px 0;
-    font-size: 0.8rem;
-
-    .f2 {
-        justify-content: space-between;
-
-        a {
-            color: $c2;
-            text-decoration: none;
-        }
-    }
-}
 </style>
---
+++ src/components/MyFooterSection2.vue
@@ -0,0 +1,39 @@
+<template>
+    <section id="f2">
+        <div class="container f2 d_flex">
+            <div>
+                Enjoy the low price. We are tracking any itnention of piracy.
+            </div>
+            <div>
+                &copy; 2020 NEXGEN is Pourdly Powered by <a href="#">Codings</a>.
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'MyFooterSection2'
+}
+</script>
+
+<style scoped lang="scss">
+@import '../style/vars.scss';
+@import '~@fortawesome/fontawesome-free/css/all.css';
+
+#f2 {
+    background-color: $c15;
+    color: $c1;
+    padding: 20px 0;
+    font-size: 0.8rem;
+
+    .f2 {
+        justify-content: space-between;
+
+        a {
+            color: $c2;
+            text-decoration: none;
+        }
+    }
+}
+</style>
