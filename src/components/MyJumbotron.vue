c76f3169c39504f1ba481e0e73be24d8d3754e9a
Luca Somaschini <somas99@gmail.com>
2022-09-07 13:42:29 +0200

    Added component in Jumbotron

--- src/components/MyJumbotron.vue
+++ src/components/MyJumbotron.vue
@@ -12,32 +12,17 @@
           </ul>
         </div>
       </div>
-      <div id="consulting">
-        <h5>FUSIONS & AQUISITIONS</h5>
-        <h1>Insurance</h1>
-        <h1>Consulting</h1>
-        <div class="input_first_row">
-          <input type="text" placeholder="Name" />
-          <input type="email" placeholder="Email" />
-        </div>
-        <div>
-          <input type="number" placeholder="Phone" />
-          <select name="info" id="info">
-            <option value="">More info</option>
-            <option value="Option1">Option1</option>
-            <option value="Option2">Option2</option>
-            <option value="Option3">Option3</option>
-          </select>
-        </div>
-        <button class="btn btn_green">GET IN TOUCH</button>
-        <button class="btn btn_white">READ MORE</button>
-      </div>
+      <MyJumbotronCard />
     </div>
 </template>
 
 <script>
+import MyJumbotronCard from './MyJumbotronCard.vue';
 export default {
     name: 'MyJumbotron',
+    components: {
+      MyJumbotronCard
+    },
     data() {
         return {
             navbarLinks: [
@@ -126,45 +111,5 @@ export default {
         }
       }
     }
-
-    #consulting {
-      position: absolute;
-      right: 15%;
-      bottom: 30%;
-
-      h5 {
-        color: $c5;
-      }
-
-      h1 {
-        font-size: 4rem;
-        font-weight: bold;
-      }
-
-      input,
-      select {
-        margin-right: 10px;
-        margin-bottom: 10px;
-        padding: 10px;
-        background-color: $c6;
-        border: 1px solid $c6;
-        border-radius: 4px;
-      }
-
-      .input_first_row {
-        margin-top: 20px;
-      }
-
-      select {
-        color: grey;
-        width: 47%;
-      }
-
-      .btn_white{
-        margin-left: 15px;
-        background-color: #e1e5e6;
-        color: $c2;
-      }
-    }
-  }
+}
 </style>
